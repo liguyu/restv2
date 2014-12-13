d6a6490d9e94d3747af64e4649735da15b40bac1
DuBin1988 <23391640@qq.com>
2014-12-13 13:49:23 +0800

    脚本对象调用时添加捕获异常

--- s2/s2DLL/Program/ObjectTools/ScriptObj.cs
+++ s2/s2DLL/Program/ObjectTools/ScriptObj.cs
@@ -10,12 +10,15 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Windows.Browser;
 using System.Json;
+using Com.Aote.Logs;
 
 namespace Com.Aote.ObjectTools
 {
     //调用脚本对象
     public class ScriptObj : GeneralObject
     {
+        private static Log Log = Log.GetInstance("Com.Aote.ObjectTools.ScriptObj");
+
         //脚本方法名
         public string Method { get; set; }
 
@@ -39,33 +42,40 @@ namespace Com.Aote.ObjectTools
         //执行方法
         public void Invoke()
         {
-            State = State.StartLoad;
-            IsBusy = true;
-            string str = Method;
-            foreach (var item in this._customPropertyValues.Keys)
+            try
             {
-                str = str.Replace("#" + item + "#", this._customPropertyValues[item] + "");
+                State = State.StartLoad;
+                IsBusy = true;
+                string str = Method;
+                foreach (var item in this._customPropertyValues.Keys)
+                {
+                    str = str.Replace("#" + item + "#", this._customPropertyValues[item] + "");
+                }
+                object o = HtmlPage.Window.Eval(str);
+                if (o is string)
+                {
+                    JsonObject item = JsonValue.Parse(o.ToString()) as JsonObject;
+                    FromJson(item);
+                    string tishi = this.GetPropertyValue("tishi") + "";
+                    string isValid = this.GetPropertyValue("isValid") + "";
+                    string result = this.GetPropertyValue("result") + "";
+                    if (isValid != null && isValid.Equals("False") || result != null && result.Equals("False"))
+                    {
+                        Error = tishi;
+                        State = State.Error;
+                    }
+                    else
+                    {
+                        State = State.End;
+                    }
+                }
+                IsBusy = false;
+                State = State.Loaded;
             }
-             object o = HtmlPage.Window.Eval(str);
-            if (o is string)
+            catch (Exception e)
             {
-                JsonObject item = JsonValue.Parse(o.ToString()) as JsonObject;
-                FromJson(item);
-                string tishi = this.GetPropertyValue("tishi")+"";
-                string isValid = this.GetPropertyValue("isValid")+"";
-                string result = this.GetPropertyValue("result")+"";
-                if (isValid != null && isValid.Equals("False") || result != null && result.Equals("False"))
-                {
-                    Error = tishi;
-                    State = State.Error;
-                }
-                else
-                {
-                    State = State.End;
-                }
+                Log.Debug("调用脚本" + Method + "异常");
             }
-            IsBusy = false;
-            State = State.Loaded;
         }
     }
 }
